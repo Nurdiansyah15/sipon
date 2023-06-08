bb13878156d5974066fbaaf26781ce784b058f3e
Nurdiansyah15 <nurdiansyahmrnurd150900@gmail.com>
2023-06-08 22:32:35 +0700

    edit migration

--- database/migrations/2023_03_25_185304_create_sec_acts_table.php
+++ database/migrations/2023_03_25_185304_create_sec_acts_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('sec_acts', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->time('start_act');
+            $table->time('end_act');
+
             $table->timestamps();
         });
     }
